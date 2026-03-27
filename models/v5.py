4b2752a4afe7d08a11d2ddc74682f3fc9fd0c3fc
kyle castillo <kyle.castillo205@gmail.com>
2026-03-27 15:32:42 -0500

    v5 done

---
+++ models/v5.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df_with_names = pd.read_csv(
+    "../data/cleaned/combined_datasets/v5/combined_dataset_v5.csv"
+)
+df = pd.read_csv("../data/cleaned/combined_datasets/v5/ml_dataset_v5.csv")
