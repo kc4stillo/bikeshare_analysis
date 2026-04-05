138da2b6b9f3fc540eb0da1ef16579736ecaaaca
kyle castillo <kyle.castillo205@gmail.com>
2026-04-04 20:17:58 -0500

    model 7

---
+++ models/v7.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+total_df = pd.read_csv("../data/cleaned/combined_datasets/v7/combined_dataset_v7.csv")
+df = pd.read_csv("../data/cleaned/combined_datasets/v7/combined_dataset_v7.csv")
